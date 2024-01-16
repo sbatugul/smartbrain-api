6f17f2c2605993507444067e74c2b9e9b357a0b1
sbatugul <147319081+sbatugul@users.noreply.github.com>
2024-01-16 18:26:03 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bcrypt from "bcrypt-nodejs";
+import cors from "cors";
+import knex from "knex";
+import { text } from "stream/consumers";
+import register from "./controllers/register.js";
+import signin from "./controllers/signin.js";
+import profile from "./controllers/profile.js";
+import image from "./controllers/image.js";
+
+
+const db = knex ({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password: 'batu',
+      database : 'smart_brain'
+    }
+  });
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {res.send(database.users);})
+app.post('/signin',(req, res) => {signin.handleSignIn(req, res, bcrypt, db)}) 
+app.post('/register',(req,res) => {register.handleRegister(req, res, bcrypt, db)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
